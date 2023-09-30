ec29dc41d8ca33718bfab1f6c09cf8e440d1fb76
DongJunHan <gamedokdok@naver.com>
2023-09-30 13:35:08 +0900

    refactor: 외부에서 설정값 가지고 오도록 수정

--- main.py
+++ main.py
@@ -4,27 +4,34 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import yaml
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
-
-templates = Jinja2Templates(directory="client/public")
-templates.env.globals["STATIC_URL"] = "/client/public"
-
 class Login(BaseModel):
     clientId: str
-    passcode: any
+    passcode: str
 class CustomFastAPI(FastAPI):
     def __init__(self, *args, **kwargs):
-        super(CustomFastAPI, self).__init__(*agrs, **kwargs)
+        super(CustomFastAPI, self).__init__(*args, **kwargs)
         self.base_url = None
-        self.resource_url = None
+        self.resource_path = None
+        self.get_yaml_data()
     def load_yaml(self):
         with open('application.yml') as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
         return data
 
+    def get_yaml_data(self):
+        yaml_data = self.load_yaml()
+        self.base_url = yaml_data['base_url']
+        self.resource_path = yaml_data['resource_path']
+
 app = CustomFastAPI()
-app.mount("/client/public", StaticFiles(directory=os.path.join(BASE_DIR, "client/public")), 
-name="client/pulbic")
+
+templates = Jinja2Templates(directory=app.resource_path)
+templates.env.globals["STATIC_URL"] = "/"+app.resource_path
+
+app.mount("/"+app.resource_path, StaticFiles(directory=os.path.join(BASE_DIR, app.resource_path)), 
+name=app.resource_path)
 
 @app.get("/")
 async def root(request : Request):
