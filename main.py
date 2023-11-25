a05b173926b38411961f2954218a7e527f78e0e2
innosonian-david <gamedokdok@naver.com>
2023-11-25 14:13:35 +0900

    adjust  python pep8 formatter

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.post('/login')
 async def login(login_schema: dict):
     return login_schema['id']
--- main_test.py
+++ main_test.py
@@ -1,19 +1,21 @@
 from fastapi.testclient import TestClient
 from fastapi import status
+
 from main import app
+
 client = TestClient(app)
 
+
 def test_login():
-    #given
+    # given
     id = "asd"
     password = "csa"
     form = {
         "id": id,
         "password": password
     }
-    #when
+    # when
     response = client.post("/login", json=form)
-    #then
+    # then
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == id
-    
